51840027b098dd7d739128122c2aaeea77530539
Lucas Tétreault <lucastetreault@gmail.com>
2017-08-29 21:46:20 -0700

    run local config and a bit of data cleansing

--- results/index.js
+++ results/index.js
@@ -6,24 +6,24 @@ const results = file.readFileSync(path.resolve(__dirname, 'results.csv'))
   .split('\n')
   .map(row => row.split('|').map(column => column.toString().trim()).map(column => column === '&nbsp;' ? '' : column))
   .map(res => ([
-    res[0],
-    res[1] ? new Date(res[1]) : new Date(0),
-    res[2],
-    res[3],
-    res[4],
-    res[5],
-    res[6],
-    parseFloat(res[7]) || 0,
-    res[8],
-    res[9],
-    res[10],
-    parseFloat(res[11]) || 0,
-    parseFloat(res[12]) || 0,
-    parseFloat(res[13]) || 0,
-    parseFloat(res[14]) || 0,
-    parseFloat(res[15]) || 0,
-    parseInt(res[16]) || 0,
-    res[17] !== 'yes'
+    res[0],                                     //'competition',
+    res[1] ? new Date(res[1]) : new Date(0),    //'date',
+    res[2],                                     //'location',
+    res[3],                                     //'competitionType',
+    res[4],                                     //'sex',
+    res[5],                                     //'name',
+    res[6],                                     //'province',
+    parseFloat(res[7]) || 0,                    //'weight',
+    res[8],                                     //'oldWeightClass',
+    res[9],                                     //'weightClass',
+    res[10],                                    //'ageCategory',
+    parseFloat(res[11]) || 0,                   //'squat',
+    parseFloat(res[12]) || 0,                   //'bench',
+    parseFloat(res[13]) || 0,                   //'deadlift',
+    parseFloat(res[14]) || 0,                   //'total',
+    parseFloat(res[15]) || 0,                   //'wilks',
+    parseInt(res[16]) || 0,                     //'year',
+    res[17] !== 'yes'                           //'equipped'
   ]))
 
 module.exports = results
--- src/index.js
+++ src/index.js
@@ -9,8 +9,15 @@ app.get('/', async (req, res) => {
   const client = new Client({connectionString: process.env.DATABASE_URL})
 
   await client.connect()
-  const results = await client.query('SELECT * FROM results ORDER BY wilks DESC LIMIT 100')
-  res.json(results)
+  const results = await client.query('SELECT * FROM results ORDER BY wilks DESC')
+  const reduced = Array.from(results.rows.reduce((prev, curr) => {
+    if (!prev.has(curr.name)) {
+      console.log(curr.name)
+      prev.set(curr.name, curr)
+    }
+    return prev
+  }, new Map()).values()).slice(0, 100)
+  res.json(reduced)
   await client.end()
 })
 
